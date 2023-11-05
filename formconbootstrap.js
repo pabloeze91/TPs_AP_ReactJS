4991a1e1e792bfc1a21a72f2cf7832811ad546ee
Pablo Schlotthauer <126608539+pabloeze91@users.noreply.github.com>
2023-11-04 23:57:01 -0300

    Delete formconbootstrap.js

--- formconbootstrap.js
+++
@@ -1,6 +0,0 @@
-const estado = document.getElementById("estado");
-const estadoSeleccionado = document.getElementById("estadoSeleccionado");
-
-estado.addEventListener("change", function () {
-  estadoSeleccionado.textContent = this.value;
-});
